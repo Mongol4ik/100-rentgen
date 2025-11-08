0c1b60e17793fe72e761ac7dcddcb3ff59dbf962
Mongol4ik <vova.belyaev357@gmail.com>
2025-11-08 13:51:21 +0100

    Create config-template.js

---
+++ config-template.js
@@ -0,0 +1,31 @@
+const CONFIG = {
+    JSONBIN: {
+        BASE_URL: 'https://api.jsonbin.io/v3/b',
+        MASTER_KEY: 'ВАШ_MASTER_KEY_ЗДЕСЬ',
+        ACCESS_KEY: 'ВАШ_ACCESS_KEY_ЗДЕСЬ',
+        BIN_IDS: {
+            PRODUCTS: 'ВАШ_BIN_ID_ПРОДУКТЫ',
+            USERS: 'ВАШ_BIN_ID_ПОЛЬЗОВАТЕЛИ', 
+            CATS: 'ВАШ_BIN_ID_КАТЕГОРИИ',
+            CHATS: 'ВАШ_BIN_ID_ЧАТЫ',
+            CONFIG: 'ВАШ_BIN_ID_КОНФИГ',
+            BETS: 'ВАШ_BIN_ID_СТАВКИ',
+            FIGHTS: 'ВАШ_BIN_ID_БОИ'
+        }
+    },
+    APP: {
+        NAME: '100 Рентген',
+        VERSION: '1.0.0',
+        DEFAULT_SELLER_PASSWORD: 'admin123'
+    },
+    STORAGE_KEYS: {
+        CURRENT_USER: 'currentUser',
+        PRODUCTS: 'products_v_final_v1',
+        USERS: 'users_v_final_v1',
+        CATS: 'cats_v_final_v1',
+        CHATS: 'chats_v1',
+        SELLER_CONFIG: 'sellerConfig',
+        BETS: 'bets_v1',
+        FIGHTS: 'fights_v1'
+    }
+};
